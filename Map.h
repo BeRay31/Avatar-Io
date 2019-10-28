913796caae0fd71844d24c5da53b3c40f7537473
BeRay31 <13518136@std.stei.itb.ac.id>
2019-10-29 01:09:27 +0700

    Added Map ADT

--- Map.h
+++ Map.h
@@ -4,13 +4,33 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-//#include "matriks.h"
+#include "matriks.h"
 #include "Buildings.h"
+#include "infofileeksternal.h"
+#include "Graph.h"
+#include "pcolor.h"
 
-#define MaxRow 20
-#define MinRow 10
-#define MaxCol 30
-#define MinCol 10
+typedef int indeks; /* indeks baris, kolom */
+typedef Buildings ElType;
+typedef struct {
+    ElType M[BrsMax+1][KolMax+1];
+    GraphArr *G;
+    int NBrsEff; /* banyaknya/ukuran baris yg terdefinisi */
+    int NKolEff; /* banyaknya/ukuran kolom yg terdefinisi */
+} BuildMap;
+
+void MakeBMap(BuildMap *Map);
+/*
+{I.S Anything}
+{F.S Matrix of Building Initiated}
+*/
+
+void PrintMap(BuildMap Map);
+/*
+{I.S Map defined}
+{F.S Map Printed}
+*/
+
 
 
 #endif
