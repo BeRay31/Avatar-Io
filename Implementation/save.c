fac76b02b0759cd21241de305e3c1fc75b626823
qurrata111 <qurrata111@gmail.com>
2019-11-24 16:36:05 +0700

    nambahin sesuatu di quit

--- Implementation/save.c
+++ Implementation/save.c
@@ -4,15 +4,11 @@
 #include "../include1/loadinfoawal.h"
 #include "../include1/save.h"
 
-// gcc driverFile.c mesinkar.c mesinkata.c loadinfoawal.c Buildings.c point.c matriks.c save.c -o run
 
 void Save (TabBuildings CurrentBuilding, int N, int M, int NBuilding, Queue q1, Queue q2, int Turn)
-// p.s. dalam main program dipake
-// include header dari loadinfoawal
-// untuk dapet position
 {
 	/* open file for writing */
-	fp = fopen("driver-save.dat","w");
+	fp = fopen("save.dat","w");
 	/* write into file */
 	/* write ukuran peta */
 	fprintf(fp, "%d ", N);
@@ -34,16 +30,6 @@ void Save (TabBuildings CurrentBuilding, int N, int M, int NBuilding, Queue q1, 
 		fprintf(fp, "%d ", CurrentBuilding.TI[i].position.X);
 		fprintf(fp, "%d\n", CurrentBuilding.TI[i].position.Y);
 	}
-	/* write graf keterhubungan 
-	for (int i=1; i<=Graf.NBrsEff; i++){
-		for (int j=1; j<=Graf.NKolEff; j++){
-			if (j==Graf.NKolEff) {
-				fprintf(fp, "%d\n",Graf.Mem[i][j]);
-			} else {
-				fprintf(fp, "%d ",Graf.Mem[i][j]);
-			}
-		}
-	} */
 	/* menyimpan queue */
 	// q1
 	fprintf(fp, "%d\n", NBElmtQ(q1));
